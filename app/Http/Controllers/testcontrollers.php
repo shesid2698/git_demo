b3a7f5e30029d4b3bdb659e0e634b577ff0bf409
韋賢 <shesid2698@gmail.com>
2022-07-25 10:41:54 +0800

    新增一個function

--- app/Http/Controllers/testcontrollers.php
+++ app/Http/Controllers/testcontrollers.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class testcontrollers extends Controller
 {
-    //
+    public function index()
+    {
+        return 'index';
+    }
 }
